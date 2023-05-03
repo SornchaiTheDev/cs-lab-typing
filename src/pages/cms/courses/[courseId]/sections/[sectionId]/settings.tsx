5c44c3c9f6261b51a73e4bf6d955344154bd3539
SornchaiTheDev <sornchaithedev@gmail.com>
2023-05-03 18:25:49 +0700

    Add : History and Settings

--- src/pages/cms/courses/[courseId]/sections/[sectionId]/settings.tsx
+++ src/pages/cms/courses/[courseId]/sections/[sectionId]/settings.tsx
@@ -1,5 +1,5 @@
 import CourseLayout from "@/Layout/CourseLayout";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Input from "@/components/Common/Input";
 import { useForm } from "react-hook-form";
@@ -10,6 +10,9 @@ import Button from "@/components/Common/Button";
 import Multiple from "@/components/Search/Multiple";
 import DeleteAffect from "@/components/DeleteAffect";
 import SectionLayout from "@/Layout/SectionLayout";
+import Select from "@/components/Common/Select";
+import { AddSectionSchema, TAddSection } from "@/forms/AddSection";
+import { semesters } from "@/__mock__";
 
 interface Props {
   course: {
@@ -23,15 +26,75 @@ function Settings({ course }: Props) {
   const {
     register,
     handleSubmit,
-    formState: { errors },
-  } = useForm<TAddCourse>({
-    resolver: zodResolver(AddCourseSchema),
+    formState: { errors, isSubmitted },
+  } = useForm<TAddSection>({
+    resolver: zodResolver(AddSectionSchema),
   });
+
+  const [instructors, setInstructors] = useState<string[]>([]);
+  const [isInstructorsError, setIsInstructorsError] = useState(false);
+  const [semester, setSemester] = useState<string | null>(null);
+  const [isSemesterError, setIsSemesterError] = useState(false);
+
+  const addSection = (formData: TAddSection) => {
+    const { name, note } = formData;
+  };
+
+  useEffect(() => {
+    if (isSubmitted) {
+      if (instructors.length === 0) setIsInstructorsError(true);
+      else setIsInstructorsError(false);
+
+      if (!semester) setIsSemesterError(true);
+      else setIsSemesterError(false);
+    }
+  }, [instructors, semester, isSubmitted]);
 
   return (
     <SectionLayout title="12 (F 15 - 17)">
       <div className="w-1/2 p-4">
-        <div className="mt-0">
+        <div className="w-full">
+          <h4 className="text-xl">General</h4>
+          <hr className="my-2" />
+
+          <form
+            className="flex flex-col gap-2"
+            onSubmit={handleSubmit(addSection)}
+          >
+            <Select
+              options={semesters}
+              title="Semester"
+              value={semester}
+              onChange={setSemester}
+              isError={isSemesterError}
+              error="Semester cannot be empty"
+            />
+            <Input
+              title="Name"
+              label="name"
+              register={register}
+              error={errors.name && errors.name.message}
+              isError={errors.name !== undefined}
+              className="flex-1"
+            />
+
+            <Multiple
+              title="Instructors"
+              value={setInstructors}
+              isError={isInstructorsError}
+              error="Instructors cannot be empty"
+            />
+            <Input title="Note" label="note" register={register} optional />
+
+            <Button
+              icon="solar:add-circle-line-duotone"
+              className="py-3 shadow bg-sand-12 text-sand-1 active:bg-sand-11"
+            >
+              Edit
+            </Button>
+          </form>
+        </div>
+        <div className="mt-10">
           <h4 className="text-xl text-red-9">Danger Zone</h4>
           <hr className="my-2" />
           <Button
