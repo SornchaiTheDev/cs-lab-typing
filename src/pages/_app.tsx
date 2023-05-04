cbe8cc314db62a7b575da159f4bcbb68146c84fc
SornchaiTheDev <sornchaithedev@gmail.com>
2023-05-04 21:27:58 +0700

    Add : vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import "@/components/DatePicker/styles.css";
 import CommandPallete from "@/components/CommandPallete";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* <CommandPallete /> */}
+      <Analytics />
       <ToastContainer />
       <Component {...pageProps} />
     </>
