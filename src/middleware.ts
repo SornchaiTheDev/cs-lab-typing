d4445c443fe1fc938736fd45957678c7b4448a30
SornchaiTheDev <sornchaithedev@gmail.com>
2023-05-14 12:06:03 +0700

    Fix : middleware vercel forget to change .env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { withAuth } from "next-auth/middleware";
-import { getToken } from "next-auth/jwt";
 import { PATH } from "@/constants/PATH";
 
 export default withAuth({
@@ -8,22 +7,21 @@ export default withAuth({
     error: "/login",
   },
   callbacks: {
-    async authorized({ req }) {
-      const token = await getToken({ req, secret: process.env.JWT_SECRET });
-      return !!token;
-      // const { nextUrl } = req;
+    async authorized({ req, token }) {
+      if (!token) return false;
+      const { nextUrl } = req;
 
-      // const resolvePath = PATH.find(
-      //   ({ pathname }) => nextUrl.pathname === pathname
-      // );
+      const resolvePath = PATH.find(
+        ({ pathname }) => nextUrl.pathname === pathname
+      );
 
-      // if (resolvePath === undefined) return true;
+      if (resolvePath === undefined) return true;
 
-      // const isAuthorized = resolvePath?.roles.some((role) =>
-      //   token?.roles!.includes(role)
-      // );
+      const isAuthorized = resolvePath?.roles.some((role) =>
+        token?.roles!.includes(role)
+      );
 
-      // return isAuthorized;
+      return isAuthorized;
     },
   },
 });
