040e2d0a0ba55dfd34b307ea3b149496c70982e0
SornchaiTheDev <sornchaithedev@gmail.com>
2023-06-12 14:16:41 +0700

    Update : Status Modal Width

--- src/pages/cms/courses/[courseId]/sections/[sectionId]/status.tsx
+++ src/pages/cms/courses/[courseId]/sections/[sectionId]/status.tsx
@@ -42,7 +42,7 @@ const RecentTasks = ({
   return (
     <Modal
       title="Recent submissions"
-      className="h-full max-h-[80%] max-w-[50rem]"
+      className="h-full max-h-[80%] max-w-[48rem]"
       onClose={onClose}
       isOpen
     >
