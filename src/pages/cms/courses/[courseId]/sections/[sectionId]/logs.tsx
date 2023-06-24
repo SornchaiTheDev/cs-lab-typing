6ae40478925f7fb4d966de750c6bf2f93446b67f
SornchaiTheDev <sornchaithedev@gmail.com>
2023-06-24 22:21:44 +0700

    Fix : prisma schema on task_history

--- src/pages/cms/courses/[courseId]/sections/[sectionId]/logs.tsx
+++ src/pages/cms/courses/[courseId]/sections/[sectionId]/logs.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import Table from "~/components/Common/Table";
 import type { ColumnDef, PaginationState } from "@tanstack/react-table";
 import clsx from "clsx";
@@ -14,7 +14,7 @@ import type { lab_loggers } from "@prisma/client";
 
 const Type = ({ type }: { type: string }) => {
   return (
-    <div className="flex w-full justify-center">
+    <div className="flex justify-center w-full">
       <button
         className={clsx(
           "rounded-md px-2 text-sm font-medium",
@@ -161,7 +161,7 @@ function Logger() {
         <div className="flex justify-end p-2 ">
           <button
             onClick={exportCSV}
-            className="flex items-center gap-2 rounded-lg bg-sand-12 p-2 text-sand-1 shadow active:bg-sand-11"
+            className="flex items-center gap-2 p-2 rounded-lg shadow bg-sand-12 text-sand-1 active:bg-sand-11"
           >
             <Icon icon="solar:document-text-line-duotone" />
             Export as CSV
