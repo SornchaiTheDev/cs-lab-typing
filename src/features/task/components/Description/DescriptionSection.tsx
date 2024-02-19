81ac900e514a56038f57ffebbb0426bde3d5a555
SornchaiTheDev <sornchaithedev@gmail.com>
2024-02-20 02:49:11 +0700

    fix: description markdown insertion bug

--- src/features/task/components/Description/DescriptionSection.tsx
+++ src/features/task/components/Description/DescriptionSection.tsx
@@ -1,6 +1,7 @@
 import MDXEditor from "~/components/Editor";
-import React from "react";
+import React, { useEffect } from "react";
 import useTheme from "~/hooks/useTheme";
+import type { MDXEditorMethods } from "@mdxeditor/editor";
 
 interface Props {
   description: string;
@@ -10,12 +11,19 @@ interface Props {
 
 function DescriptionSection({ diffTaskBody, description, onChange }: Props) {
   const { theme } = useTheme();
+
+  const ref = React.useRef<MDXEditorMethods>(null);
+  useEffect(() => {
+    ref.current?.setMarkdown(description);
+  }, [description]);
+  
   return (
     <>
       <h4 className="mb-4 text-3xl font-bold text-sand-12 ">Description</h4>
       <div className="-z-0 min-h-[300px] overflow-hidden rounded-lg border border-sand-6 text-sand-12 dark:bg-sand-2">
         <MDXEditor
           autoFocus
+          ref={ref}
           onChange={onChange}
           diffMarkdown={diffTaskBody}
           className={theme === "light" ? "light-theme" : "dark-theme"}
